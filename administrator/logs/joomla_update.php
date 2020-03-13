8109bd498951657336545a6a38393dd9826ac197
AKSHAYCHIDRE <akshaychidre0@gmail.com>
2020-03-13 15:45:49 +0530

    updated joomla project setup

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-03-12 05:08:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-03-12T05:08:44+00:00	INFO ::1	update	Update started by user Super User (566). Old version is 3.9.9.
+2020-03-12T05:08:51+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.16-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20200312%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200312T050846Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=89b4f3033d950d89544cfac8ae27c485e067144295533c392a70cd851b5c71eb.
